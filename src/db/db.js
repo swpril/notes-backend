8be4b60d6f41b78de4966523fd512e3e3b9bf3a9
Swpril Ahuja <ahuja.s0007@gmail.com>
2021-03-13 07:06:09 +0530

    Fix environments

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-mongoose.connect(
-  'mongodb+srv://swpril:admin@cluster0.rdh4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }
-);
+mongoose.connect(process.env.MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
